e919485727a235aa02f015072135fbcfae6276fe
Sam Coffey <samcoffey7@yahoo.com>
2017-09-08 09:27:22 -0400

    Compiled Assets

--- public/assets/application-add86252187dbc712d6ffd732c55d290d866e84c000bdfab1a2754d158d13f1a.js
+++ public/assets/application-add86252187dbc712d6ffd732c55d290d866e84c000bdfab1a2754d158d13f1a.js
@@ -41977,19 +41977,33 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function Calendar(sel) {
     this.element = $(sel);
-    this.tableCells = $(sel + ' td.day');
-    this.activities = $(sel + ' li.activity');
+    this.tableCells = function() { return $(sel + ' td.day'); };
+    this.activities = function() { return $(sel + ' li.activity'); };
+    this.activityFormHtml = function() { return $('#activity-form-cache').html() };
     this.initialize();
 }
 
 /**
  * Move activity from one day to another
  *
- * @param activity {jQuery} - Activity element to move
- * @param day {jQuery} - Table cell for the day to move the activity to
+ * @param replace {jQuery} - Activity element to replace
+ * @param move {jQuery} - Activity element to move
  */
-Calendar.prototype.moveActivity = function(activity, day) {
+Calendar.prototype.replace = function(replace, move) {
     var that = this;
+    var replacingDay = $(replace).parent('ul').attr('date-attr');
+    var moveId = $(replace).attr('id-attr');
+    var movingDay = $(move).parent('ul').attr('date-attr');
+    console.log('Replacing activity from day ' + replacingDay + ' to day ' + movingDay);
+    replace.remove();
+    move.removeClass('--moving');
+    $.ajax({
+        url: '/replace_activities?move_id=' + moveId + '&move=' + movingDay,
+        method: 'GET',
+        success: function() {
+            console.log('Successfully replaced days');
+        }
+    });
 };
 
 /**
@@ -42001,54 +42015,78 @@ Calendar.prototype.initialize = function() {
 };
 
 /**
+ * Setup draggables
+ */
+Calendar.prototype.setupDraggables = function() {
+    var that = this;
+    //
+    // Activity drag functionality
+    //
+    console.log("Number of activities: " + that.activities().length);
+    console.log("Number of table cells: " + that.tableCells().length);
+    that.activities().draggable({
+        helper: 'clone',
+        start: function(event, ui) {
+            ui.helper.width($(this).width()/2);
+            $(this).addClass('--replacing');
+        },
+        drag: function(event, ui) {
+            // triggered when mouse moves during drag
+        },
+        stop: function(event, ui) {
+            // triggered when element is let go of
+        }
+    });
+};
+
+Calendar.prototype.setupDroppables = function() {
+    var that = this;
+    //
+    // Table cell drop functionality
+    //
+    that.tableCells().droppable({
+        tolerance: 'pointer',
+        hoverClass: 'drag-over',
+        drop: function(event, ui) {
+            //$('.--replacing').remove();
+            $(this).find('ul')
+                .append(ui.helper.clone()
+                    .addClass('--moving')
+                    .removeClass('ui-draggable')
+                    .removeClass('ui-draggable-handle')
+                    .removeClass('ui-draggable-dragging')
+                    .css('position', 'relative')
+                    .css('left', '')
+                    .css('right', '')
+                    .css('top', '')
+                    .css('bottom', '')
+                    .css('width', '100%'));
+            that.replace($('.--replacing'), $('.--moving'));
+            that.setupDraggables();
+        },
+        over: function(event, ui) {
+            // triggered when an accepted draggable is over this
+        },
+        out: function(event, ui) {
+            // triggered when accepted draggable moves back out
+        }
+    });
+};
+
+/**
  * Setup handler events within the calendar
  */
 Calendar.prototype.setupHandlers = function() {
     var that = this;
+    that.setupDraggables();
+    that.setupDroppables();
 
     //
-    // Activity drag functionality
+    // Activity popover initialization
     //
-    console.log("Number of activities: " + that.activities.length);
-    console.log("Number of table cells: " + that.tableCells.length);
-    that.activities.draggable({
-        helper: 'clone',
-        containment: 'document',
-        start: function(event, ui) {
-            // triggered when dragging stopped
-            // i.e. change width [ ui.helper.width(x) ]
-            console.log("Drag started");
-            ui.helper.css('opacity', 0.75);
-        },
-        drag: function(event, ui) {
-            // triggered when mouse moves during drag
-            console.log("Dragging");
-        },
-        stop: function(event, ui) {
-            // triggered when element is let go of
-            console.log("Drag stopped");
-        }
-    });
-
-    //
-    // Table cell drop functionality
-    //
-    that.tableCells.droppable({
-        tolerance: 'fit',
-        hoverClass: 'drag-over',
-        drop: function(event, ui) {
-            // triggered when an accepted draggable is dropped
-            console.log("Activity dropped");
-        },
-        over: function(event, ui) {
-            // triggered when an accepted draggable is over this
-            console.log("Draggable entered");
-        },
-        out: function(event, ui) {
-            // triggered when accepted draggable moves back out
-            console.log("Draggable left");
-        }
-    });
+    // that.activities().click(function() {
+    //     $('#activity-dialog').html($(this).html()).dialog();
+    // });
 };
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -42309,6 +42347,7 @@ String.prototype.contains = function(s) {
 var HTMLHelper = new HtmlHelper();
 
 function prepareComponents() {
+
     //
     // Modals
     //
@@ -42316,6 +42355,10 @@ function prepareComponents() {
         complete: function() {
             console.log('Modal closed');
         }
+    });
+
+    $('#new-activity-btn').click(function() {
+       $('#new-activity-modal').modal('open');
     });
 
     //
@@ -42374,6 +42417,13 @@ function prepareComponents() {
     // Create new calendar
     //
     new Calendar('.simple-calendar');
+
+    //
+    // Calendar toggle
+    //
+    $('.calendar-toggle').click(function() {
+        $('.calendar-heading').html('<div class="loader"><i class="fa fa-circle-o-notch"></i></div>');
+    });
 }
 
 /**
@@ -42381,9 +42431,5 @@ function prepareComponents() {
  */
 $(document).on('turbolinks:load', function() {
     console.log("Turbolinks loaded");
-    prepareComponents();
-}).on('page:change', function() {
-    prepareComponents();
-}).ready(function() {
     prepareComponents();
 });
